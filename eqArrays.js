5b1a409ac392092e331f297a41b46ab4bc809627
Yongjia Sun <thomastank0926@gmail.com>
2022-09-21 05:21:54 +0000

    add eqArrays to lotide

---
+++ eqArrays.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  // check the lengths of two arrays, if they are different, false is returned
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  // check if each index of elements are the same, if they are different, false is returned 
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  // otherwise, two arrays are the same
+  return true;
+};
+
+
+// Test
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
