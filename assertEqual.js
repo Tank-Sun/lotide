c5cf5a8f82d62bfdead0dba61cf92ba096ad0ce6
Yongjia Sun <thomastank0926@gmail.com>
2022-09-26 21:41:08 +0000

    modularizing assertEqual and head

--- assertEqual.js
+++ assertEqual.js
@@ -8,10 +8,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-
-assertEqual('Lighthouse Labs', 'Bootcamp');
-assertEqual('Tank', 'Tank');
-assertEqual(1, 2);
-assertEqual(1, 1);
-assertEqual(1, '1');
+module.exports = assertEqual;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual('Lighthouse Labs', 'Bootcamp');
+assertEqual('Tank', 'Tank');
+assertEqual(1, 2);
+assertEqual(1, 1);
+assertEqual(1, '1');
