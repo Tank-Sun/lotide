7c30b68d1a0d97b12e32c48f22351724c31fa324
Yongjia Sun <thomastank0926@gmail.com>
2022-09-26 22:01:58 +0000

    modularizing assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  // check the lengths of two arrays, if they are different, false is returned
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  // check if each index of elements are the same, if they are different, false is returned
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-
-  // otherwise, two arrays are the same
-  return true;
-};
+const eqArrays = require('./eqArrays')
 
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
@@ -23,8 +8,4 @@ const assertArraysEqual = function(arr1, arr2) {
   }
 };
 
-// test
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [2, 4, 6]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+module.exports = assertArraysEqual;
